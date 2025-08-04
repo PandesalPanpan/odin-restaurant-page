9ceee73e58a926fe33ec4cf7d717f7657330e4c3
PandesalPanpan <petermarticio@gmail.com>
2025-08-04 09:58:23 +0800

    Feat: Add Two tabs switch logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import "./styles.css";
 import loadHomePage from "./load_home_page";
-
-// Content container
-const content = document.querySelector('#content');
-
-loadHomePage(content);
+import loadMenuPage from "./load_menu_page";
 
 /* 
 Add Event listeners for each button
 Depending on the button triggered
 Run a different module that replaces the current current with a different one
 */
+
+const buttons = document.querySelectorAll(".tab-button");
+const content = document.querySelector('#content');
+
+// Attach a listener to each and check if it contains a class or specific id
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const tabName = button.dataset.tab;
+        console.log(`This the name: ${tabName}`);
+        switchTab(tabName);
+        // Trigger a function that switches them
+    })
+})
+
+function switchTab(tab) {
+    switch(tab) {
+        case 'home':
+            loadHomePage(content);
+            break;
+        case 'menu':
+            loadMenuPage(content);
+            break;
+        case 'about':
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+// Content container
+
+loadHomePage(content);
+
---
+++ src/load_menu_page.js
@@ -0,0 +1,5 @@
+export default function loadMenuPage(contentContainer) {
+    contentContainer.innerHTML = ``;
+
+    contentContainer.innerHTML = `<h2>This is the menu page</h2>`;
+}
