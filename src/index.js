974b4cdf2178ec9cac9472bd9f5168b530e1a291
PandesalPanpan <petermarticio@gmail.com>
2025-08-04 09:30:52 +0800

    Feaet: Add working ES6 module to load home content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./styles.css";
+import loadHomePage from "./load_home_page";
 
-alert("Hello World");
+// Content container
+const content = document.querySelector('#content');
+
+loadHomePage(content);
+
+/* 
+Add Event listeners for each button
+Depending on the button triggered
+Run a different module that replaces the current current with a different one
+*/
