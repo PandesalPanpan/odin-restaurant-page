5bee048afe3936f3df42196911d2f598f7b6887f
PandesalPanpan <petermarticio@gmail.com>
2025-08-05 08:27:02 +0800

    Feat: Home Page Complete

--- src/home_page.js
+++ src/home_page.js
@@ -9,6 +9,7 @@ export default function loadHomePage(container) {
     // Title Section
     // Create an element for the title section
     const title = document.createElement('h1');
+    title.style.marginTop = '25px';
     title.style.textAlign = 'center';
     title.textContent = 'Welcome to Odin Restaurant';
     container.appendChild(title);
@@ -24,8 +25,39 @@ export default function loadHomePage(container) {
     const description = document.createElement('div');
     description.style.marginTop = '35px';
     description.textContent = `Nestled in the heart of Valhalla, Odin Restaurant invites mortals and deities alike to indulge in a heavenly fusion of divine delights and Filipino flavors.
-    Step into a cozy tropical haven, where soft lights glow like stardust and every plate is a blessing from the skies.`;
+    Step into a cozy tropical haven, where soft lights glow like stardust and every plate is a blessing from the skies.`
 
     container.appendChild(description);
+
+    const headerMustTry = document.createElement('h3');
+    headerMustTry.textContent = "Must-Try Divine Creations";
+    headerMustTry.style.marginTop = '35px';
+    container.appendChild(headerMustTry);
+
+    // Foods
+    const listContainer = document.createElement('ul');
+    const listOfItems = {
+        "Ambrosia Adobo": "The classic Filipino favorite, slow-cooked in celestial spices and bathed in honeyed garlic oil",
+        "Mana Kare-Kare": "A thick peanut stew fit for the divine, served with enchanted eggplants and golden bagoong",
+        "Nectar of the Isles": "A refreshing tropical drink said to grant bliss to those who sip it"
+    };
+
+    for (const key in listOfItems) {
+        const food = document.createElement('li') 
+        food.style.margin= "8px auto";
+        food.textContent = `${key} - ${listOfItems[key]}`;
+        listContainer.appendChild(food);
+    }
+    container.appendChild(listContainer);
+
+    // Italic
+    const italicContainer = document.createElement('div');
+    italicContainer.style.textAlign = 'center';
+    italicContainer.style.marginTop = '35px';
+    const italicMeseage = document.createElement('i');
+    italicMeseage.textContent = "Whether it’s a feast or a quiet escape, you deserve a table in the halls of gods.";
+    italicContainer.appendChild(italicMeseage);
+    container.appendChild(italicContainer);
+
 
 }
